b2626da73dedaf7a3a298d22d45151d5721c87db
ptsxdhcl <1242356987@qq.com>
2019-03-02 17:08:53 +0800

    优化首页|增加浏览历史纪录

--- dailyfresh/apps/goods/views.py
+++ dailyfresh/apps/goods/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import View
 # from goods.models import Goods
 # Create your views here.
-def index(request):
+
+
+# http://127.0.0.1:8000
+class IndexView(View):
     '''首页'''
-    return render(request, 'index.html')
+    def get(self,request):
+        '''首页'''
+        return render(request, 'index.html')
--- dailyfresh/apps/user/views.py
+++ dailyfresh/apps/user/views.py
@@ -4,12 +4,14 @@ from django.contrib.auth import authenticate,login,logout
 from django.http import HttpResponse
 from django.core.mail import send_mail
 from user.models import User,Address
+from goods.models import GoodsSKU
 from celery_tasks.tasks import send_register_active_email
 from django.views.generic import View
 from django.conf import settings
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from itsdangerous import SignatureExpired
 from utils.mixin import LoginRequiredMixin
+from django_redis import get_redis_connection
 import re
 
 
@@ -182,7 +184,19 @@ class UserInfoView(LoginRequiredMixin,View):
         user = request.user
         address = Address.objects.get_default_address(user)
         # 获取用户的历史浏览记录
-        return render(request,'user_center_info.html',{'page':'user','address':address})
+        con = get_redis_connection('default')
+        history_key = 'history_%d'%user.id
+        # 获取用户最新浏览的5个商品的id
+        sku_ids = con.lrange(history_key,0,4)
+        # 遍历获取用户浏览的商品信息
+        goods_li = []
+        for id in sku_ids:
+            goods = GoodsSKU.objects.get(id=id)
+            goods_li.append(goods)
+
+        context = {'page':'user','address':address,'goods_li':goods_li}
+
+        return render(request,'user_center_info.html',context)
 
 
 
