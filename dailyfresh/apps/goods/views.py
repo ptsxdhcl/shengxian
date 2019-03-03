4fea38737e8e40d406bf609ad344682f8a4671e1
ptsxdhcl <1242356987@qq.com>
2019-03-04 00:16:02 +0800

    首页的优化

--- dailyfresh/apps/goods/views.py
+++ dailyfresh/apps/goods/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import View
+from goods.models import GoodsType, GoodsSKU, IndexGoodsBanner,IndexPromotionBanner,IndexTypeGoodsBanner
 # from goods.models import Goods
 # Create your views here.
 
@@ -9,4 +10,28 @@ class IndexView(View):
     '''首页'''
     def get(self,request):
         '''首页'''
-        return render(request, 'index.html')
+        # 获取商品的种类信息
+        types = GoodsType.objects.all()
+
+        # 获取首页轮播商品信息
+        goods_banners = IndexGoodsBanner.objects.all().order_by('index')
+
+        # 获取首页促销活动信息
+        promotion_banners = IndexPromotionBanner.objects.all().order_by('index')
+
+        # 获取首页分类商品展示信息
+        for type in types:  # GoodsType
+            # 获取type种类首页分类商品的图片展示信息
+            image_banners = IndexTypeGoodsBanner.objects.filter(type=type, display_type=1).order_by('index')
+            # 获取type种类首页分类商品的文字展示信息
+            title_banners = IndexTypeGoodsBanner.objects.filter(type=type, display_type=0).order_by('index')
+
+            # 动态给type增加属性，分别保存首页分类商品的图片展示信息和文字展示信息
+            type.image_banners = image_banners
+            type.title_banners = title_banners
+
+        context = {'types': types,
+                   'goods_banners': goods_banners,
+                   'promotion_banners': promotion_banners}
+
+        return render(request, 'index.html',context)
