88fb9d17f71242594cfd965a3fa63815d6194068
ptsxdhcl <1242356987@qq.com>
2019-03-08 21:48:33 +0800

    详情页|列表页|全局搜索的完成

--- dailyfresh/apps/goods/models.py
+++ dailyfresh/apps/goods/models.py
@@ -31,7 +31,7 @@ class GoodsSKU(BaseModel):
     desc = models.CharField(max_length=256, verbose_name='商品简介')
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='商品价格')
     unite = models.CharField(max_length=20, verbose_name='商品单位')
-    image = models.ImageField(upload_to='goods', verbose_name='商品图片')
+    image = models.ImageField(upload_to='goo  ds', verbose_name='商品图片')
     stock = models.IntegerField(default=1, verbose_name='商品库存')
     sales = models.IntegerField(default=0, verbose_name='商品销量')
     status = models.SmallIntegerField(default=1, choices=status_choices, verbose_name='商品状态')
--- dailyfresh/dailyfresh/settings.py
+++ dailyfresh/dailyfresh/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tinymce', # 富文本编辑器
+    'haystack' ,# 全文检索框架
     'user', # 用户模块
     'goods', # 商品模块
     'cart', # 购物车模块
@@ -155,3 +156,20 @@ SESSION_CACHE_ALIAS = "default"
 
 # 配置登录url地址
 LOGIN_URL='/user/login' # /accounts/login
+
+# 全文检索框架的配置
+HAYSTACK_CONNECTIONS = {
+    'default': {
+        # 使用whoosh引擎
+        'ENGINE': 'haystack.backends.whoosh_backend.WhooshEngine',
+        # 'ENGINE': 'haystack.backends.whoosh_cn_backend.WhooshEngine',
+        # 索引文件路径
+        'PATH': os.path.join(BASE_DIR, 'whoosh_index'),
+    }
+}
+
+# 当添加、修改、删除数据时，自动生成索引
+HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
+
+# 指定搜索结果每页显示的条数
+HAYSTACK_SEARCH_RESULTS_PER_PAGE=1
